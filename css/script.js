63c7181cdb7cdd8ed8fa48d580ecdc984b15c76b
RakibHossain621 <rakibhasanrasel07@gmail.com>
2024-05-12 18:27:54 +0600

    layout resposive

--- css/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const listitems = document.querySelector('list_item');
-  
-    menuToggle.addEventListener('click', function() {
-        listitems.listitems.toggle('active');
-    });
-  });
